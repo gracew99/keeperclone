39510b0c055b8ae2bf3bd7bc5f155e042aa71bf7
gracew99 <7.knicksfan.7@gmail.com>
2020-06-11 13:24:24 -0400

    render all notes using props and mapping

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Note from "./Note.jsx";
+import notes from "../notes.js";
 
 function App(){
 
+
+    function createNote(obj){
+        return <Note 
+            key={obj.key}
+            title={obj.title}
+            content={obj.content}
+        />;
+    }
+
     return (<div>
         <Header />
-        <Note />
+        {notes.map(createNote)}
         <Footer />
     </div>);
 
